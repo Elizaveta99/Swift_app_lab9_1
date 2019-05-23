697b3e218b11d3b46e9e69d23417d7111e7d522e
eliz <eliz.vitebsk@gmail.com>
2019-05-23 11:11:21 -0700

    enter&registration work

--- application_1/Controller/ViewController.swift
+++ application_1/Controller/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
     @IBOutlet var agreeLabel: UILabel!
     @IBOutlet var agreeSwitch: UISwitch!
     @IBOutlet var enterButton: UIButton!
+    @IBOutlet var onlineCoursersView: UICollectionView!
+    @IBOutlet var descriptionLabel: UILabel!
     
     
     @IBAction func chooseSwitched(_ sender: Any) {
@@ -25,24 +27,57 @@ class ViewController: UIViewController {
         {
             secondView.isHidden = true
             enterButton.setTitle("Войти", for: .normal)
+            enterButton.isEnabled = true
         }
         else
         {
             secondView.isHidden = false
             enterButton.setTitle("Зарегистрироваться", for: .normal)
+            enterButton.isEnabled = false
         }
         login.text = ""
         password.text = ""
         passwordAgain.text = ""
-        enterButton.isEnabled = false
         agreeSwitch.isOn = false
     }
     
+    @IBAction func agreeSwitchTapped(_ sender: Any) {
+        if choose.selectedSegmentIndex == 1 {
+            enterButton.isEnabled = !enterButton.isEnabled
+        }
+    }
     
+    @IBAction func enterButtonTapped(_ sender: Any) {
+        if choose.selectedSegmentIndex == 0
+        {
+            let savedPassword = UserDefaults.standard.object(forKey: self.login.text!)
+            if savedPassword != nil
+            {
+                if ((savedPassword! as! String).elementsEqual(password.text!)) == true
+                {
+                    onlineCoursersView.isHidden = false
+                    descriptionLabel.isHidden = false
+                }
+            }
+        }
+        else
+        {
+            if ((login.text?.elementsEqual(""))! || (password.text?.elementsEqual(""))! || (passwordAgain.text?.elementsEqual(""))!) == false
+            {
+                if (passwordAgain.text)!.elementsEqual(password.text!)
+                {
+                    UserDefaults.standard.set(password.text, forKey: login.text!)
+                    onlineCoursersView.isHidden = false
+                    descriptionLabel.isHidden = false
+                }
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         secondView.isHidden = true
+        onlineCoursersView.isHidden = true
     }
 
 
