204226eb37f4c66ccac9dc70764931809170c571
eliz <eliz.vitebsk@gmail.com>
2019-05-23 08:27:28 -0700

    Initial Commit

---
+++ application_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  application_1
+//
+//  Created by Lizaveta Rudzko on 3/2/1398 AP.
+//  Copyright © 1398 Lizaveta Rudzko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
