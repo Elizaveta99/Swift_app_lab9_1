f2cae59fa47c1d821d44b344404864ad164ff926
eliz <eliz.vitebsk@gmail.com>
2019-05-23 10:09:55 -0700

    all view with registration added

--- application_1/Controller/ViewController.swift
+++ application_1/Controller/ViewController.swift
@@ -10,9 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var secondView: UIView!
+    @IBOutlet var choose: UISegmentedControl!
+    @IBOutlet var login: UITextField!
+    @IBOutlet var password: UITextField!
+    @IBOutlet var passwordAgain: UITextField!
+    @IBOutlet var agreeLabel: UILabel!
+    @IBOutlet var agreeSwitch: UISwitch!
+    @IBOutlet var enterButton: UIButton!
+    
+    
+    @IBAction func chooseSwitched(_ sender: Any) {
+        if choose.selectedSegmentIndex == 0
+        {
+            secondView.isHidden = true
+            enterButton.setTitle("Войти", for: .normal)
+        }
+        else
+        {
+            secondView.isHidden = false
+            enterButton.setTitle("Зарегистрироваться", for: .normal)
+        }
+        login.text = ""
+        password.text = ""
+        passwordAgain.text = ""
+        enterButton.isEnabled = false
+        agreeSwitch.isOn = false
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        secondView.isHidden = true
     }
 
 
